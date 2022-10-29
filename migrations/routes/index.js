ddb1fa401c9081850248a78baab83d7785ee089b
LAPTOP-PQPCKK5H\alstj <alstjq123579@daum.net>
2022-10-29 15:29:04 +0900

    김민섭: 배포중 module_not_found버그로 인한 재 푸쉬 22-10-29 15:29

---
+++ migrations/routes/index.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+const membersRouter = require("../routes/members");
+
+router.use("/members", membersRouter);
+module.exports = router;
