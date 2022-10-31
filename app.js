8afd0909d6661a4df06a86e235ec64caecef5837
Damiyd <dpekfa144@gmail.com>
2022-10-31 14:44:25 +0900

    최예닮 app.js에 socket 추가 22.10.31 14:44분

--- app.js
+++ app.js
@@ -10,6 +10,47 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use("/", indexRouter);
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/get_messages', (req, res) => {
+  connection.query('SELECT * FROM messages', (error, messages) => {
+    res.end(JSON.stringify(messages))
+  })
+})
+
+const http = require('http').createServer(app)
+
+const io = require('socket.io')(http)
+const mysql = require('mysql')
+
+const connection = mysql.createConnection({
+host: 'express-database.cdwl0uwak8rz.ap-northeast-2.rds.amazonaws.com',
+user: 'root',
+password: '4321aaaa',
+database: '7weeks_socket',
+})
+
+io.on("connection", (socket) => {
+  console.log("User connected", socket.id);
+  socket.on("new_message", (data) => {
+      console.log("Client says", data);
+
+  io.emit('new_message', data)
+
+  connection.query(
+      "INSERT INTO messages (message) VALUES ('" + data + "')",
+    )
+    })
+  });
+
+const port = 3000
+
+http.listen(port, () => {
+console.log(`Listening to port ${port}`)
+})
+
 app.listen(PORT, () => {
   console.log(`localhost:${PORT} is Running`);
 });
