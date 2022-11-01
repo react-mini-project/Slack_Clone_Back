7d2e015e77473f12852ae0c91e3b8b3b6cd81a21
LAPTOP-PQPCKK5H\alstj <alstjq123579@daum.net>
2022-11-01 18:11:54 +0900

    김민섭: updateMember 기능 완성 22-11-01 18:11

--- repository/member.js
+++ repository/member.js
@@ -2,16 +2,27 @@ const { Users } = require("../models");
 
 class MembersRepository {
   createMembers = async (email, profileName) => {
-    const createMembers = await Users.create({
-      email,
-      profileName,
-    });
-    return createMembers;
+    try {
+      const createMembers = await Users.create({
+        email,
+        profileName,
+      });
+      return createMembers;
+    } catch (err) {
+      throw new Error();
+    }
   };
 
   updateMember = async (email, profileName, nickname) => {
-    const updateMember = await Users.update({ nickname }, { where: { email } });
-    return updateMember;
+    try {
+      const updateMember = await Users.update(
+        { nickname },
+        { where: { email } }
+      );
+      return updateMember;
+    } catch (err) {
+      throw new Error();
+    }
   };
 
   deleteMember = async () => {};
