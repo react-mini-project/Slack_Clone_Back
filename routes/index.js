3439aa7ebe38a3d6fd158fc41efc7d385cf498ff
LAPTOP-PQPCKK5H\alstj <alstjq123579@daum.net>
2022-11-02 16:43:09 +0900

    김민섭: 소켓 백엔드 구현 완료 22-11-02 16:43

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const membersRouter = require("../routes/members");
+const chatsRouter = require("./chats");
+const roomRouter = require("./room");
 
+router.use("/room", roomRouter);
 router.use("/members", membersRouter);
-
+router.use("/chats", chatsRouter);
 module.exports = router;
